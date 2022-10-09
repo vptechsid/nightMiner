308fd7f7a1399d86b0445a60ce24e0608d615535
Rahul Kumar Gupta <rahulguptanitro@gmail.com>
2022-10-09 21:51:02 +0530

    Multivalidate

--- nightMiner.py
+++ nightMiner.py
@@ -63,7 +63,10 @@ while True:
                     # if this is a new ship, we'll be in the except, below
                     if glo.Variables.early_blockade_processing and \
                         glo.Variables.current_assignments[ship.id].primary_mission == glo.Missions.early_blocka7de:
-                        glo.Misc.log_w_shid('core', 'info', ship.id, " entering early_blockade()")
+                            command_queue.append(seek_n_nav.Offense.early_blockade(me, ship, game, game_map, turn))
+                            glo.Misc.log_w_shid('core', 'info', ship.id, " entering early_blockade()")
+                            continue
+                        
                         
 
                 if glo.Variables.current_assignments[ship.id].primary_mission == glo.Missions.mining:
